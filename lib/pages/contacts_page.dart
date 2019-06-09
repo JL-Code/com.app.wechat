630f40b01d87a089273fcce5c41336f0a5be1beb
jiangy <jiangliu0316@outlook.com>
2019-06-09 21:16:54 +0800

    完成对index_page 的 页面切换改造

---
+++ lib/pages/contacts_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ContactsPage extends StatelessWidget {
+  static const String routeName = "/contacts";
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Text("ContactsPage"),
+    );
+  }
+}
